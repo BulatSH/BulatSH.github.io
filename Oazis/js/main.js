12445c8cb01cb1f0b39ee872ada51ff3686e26ad
Шарафутдинов Булат Равилевич <bulat@MacBook-Air-Sarafutdinov.local>
2019-10-18 21:29:09 +0300

    fix Oazis

--- Oazis/js/main.js
+++ Oazis/js/main.js
@@ -4,44 +4,25 @@ $(document).ready(function () {
     let informationDisclosureItem = $('.information__disclosure-item');
     let informationDisclosureIcon = $('.information__disclosure-icon');
 
-    let clickinformationDisclosureItem = [
-        {
-            select: false
-        },
-        {
-            select: false
-        },
-        {
-            select: false
-        },
-        {
-            select: false
-        },
-        {
-            select: false
-        },
-        {
-            select: false
-        },
-        {
-            select: false
-        }
-    ]
+    let clickinformationDisclosureItem = []
+    for (let i = 0; i < informationDisclosureBtn.length; i++) {
+        clickinformationDisclosureItem[i] = false;
+    }
 
     let informationDisclosure = function (i) {
         $(informationDisclosureBtn[i]).click(function () {
-            if (!clickinformationDisclosureItem[i].select) {
+            if (!clickinformationDisclosureItem[i]) {
                 $(informationDisclosureItem[i]).css('background-color', '#5c9800');
                 $(informationDisclosureItem[i]).css('color', '#fff');
                 $(informationDisclosureIcon[i]).css('background-color', '#4a7b00');
                 $(informationDisclosureIcon[i]).addClass('information__disclosure-icon__click');
-                clickinformationDisclosureItem[i].select = true;
-            } else if (clickinformationDisclosureItem[i].select) {
+                clickinformationDisclosureItem[i] = true;
+            } else if (clickinformationDisclosureItem[i]) {
                 $(informationDisclosureItem[i]).css('background-color', '#f3f1f1');
                 $(informationDisclosureItem[i]).css('color', '#000');
                 $(informationDisclosureIcon[i]).css('background-color', '#e9e6e6');
                 $(informationDisclosureIcon[i]).removeClass('information__disclosure-icon__click');
-                clickinformationDisclosureItem[i].select = false;
+                clickinformationDisclosureItem[i] = false;
             }
 
             $(this).parent().find(informationDisclosureList[i]).slideToggle(200);
