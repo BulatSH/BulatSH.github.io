6b74d8ecac05318949cf53f1d1d6e0ca06265074
Шарафутдинов Булат Равилевич <bulat@MacBook-Air-Sarafutdinov.local>
2019-10-18 17:57:42 +0300

    addOazis

---
+++ Oazis/js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    let informationDisclosureBtn = $('.information__disclosure-item__wrapper');
+    let informationDisclosureList = $('.information__disclosure-list');
+    let informationDisclosureItem = $('.information__disclosure-item');
+    let informationDisclosureIcon = $('.information__disclosure-icon');
+
+    let clickinformationDisclosureItem = [
+        {
+            select: false
+        },
+        {
+            select: false
+        },
+        {
+            select: false
+        },
+        {
+            select: false
+        },
+        {
+            select: false
+        },
+        {
+            select: false
+        },
+        {
+            select: false
+        }
+    ]
+
+    let informationDisclosure = function (i) {
+        $(informationDisclosureBtn[i]).click(function () {
+            if (!clickinformationDisclosureItem[i].select) {
+                $(informationDisclosureItem[i]).css('background-color', '#5c9800');
+                $(informationDisclosureItem[i]).css('color', '#fff');
+                $(informationDisclosureIcon[i]).css('background-color', '#4a7b00');
+                $(informationDisclosureIcon[i]).addClass('information__disclosure-icon__click');
+                clickinformationDisclosureItem[i].select = true;
+            } else if (clickinformationDisclosureItem[i].select) {
+                $(informationDisclosureItem[i]).css('background-color', '#f3f1f1');
+                $(informationDisclosureItem[i]).css('color', '#000');
+                $(informationDisclosureIcon[i]).css('background-color', '#e9e6e6');
+                $(informationDisclosureIcon[i]).removeClass('information__disclosure-icon__click');
+                clickinformationDisclosureItem[i].select = false;
+            }
+
+            $(this).parent().find(informationDisclosureList[i]).slideToggle(200);
+            return false;
+        });
+    }
+
+    for (let i = 0; i < informationDisclosureBtn.length; i++) {
+        informationDisclosure(i);
+    }
+});
