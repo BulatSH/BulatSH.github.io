3cd0037b46a6ab8e832526990c235c75a01af3bd
Булат <hufdgdgdgfd@mail.ru>
2020-12-09 05:26:40 +0300

    fix 4

--- binpart/script/smoothScroll.js
+++ binpart/script/smoothScroll.js
@@ -1,18 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const anchors = document.querySelectorAll('a[href*="#"]');
-
-//     for (let anchor of anchors) {
-//         anchor.addEventListener("click", function(event) {
-//             event.preventDefault();
-//             const blockID = anchor.getAttribute('href');
-//             document.querySelector('' + blockID).scrollIntoView({
-//                 behavior: "smooth",
-//                 block: "start",
-//             });
-//         })
-//     }
-// });
-
 $(document).ready(function() {
     $('a[href*="#"]').click(function() {
         $("html, body").animate({
