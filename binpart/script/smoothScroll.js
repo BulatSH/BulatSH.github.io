fc1ee810cee269d929ee82ef23182c2293fa1b7f
Булат <hufdgdgdgfd@mail.ru>
2020-12-09 05:23:46 +0300

    fix scroll

--- binpart/script/smoothScroll.js
+++ binpart/script/smoothScroll.js
@@ -1,14 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const anchors = document.querySelectorAll('a[href*="#"]');
+// document.addEventListener("DOMContentLoaded", () => {
+//     const anchors = document.querySelectorAll('a[href*="#"]');
 
-    for (let anchor of anchors) {
-        anchor.addEventListener("click", function(event) {
-            event.preventDefault();
-            const blockID = anchor.getAttribute('href');
-            document.querySelector('' + blockID).scrollIntoView({
-                behavior: "smooth",
-                block: "start",
-            });
-        })
-    }
+//     for (let anchor of anchors) {
+//         anchor.addEventListener("click", function(event) {
+//             event.preventDefault();
+//             const blockID = anchor.getAttribute('href');
+//             document.querySelector('' + blockID).scrollIntoView({
+//                 behavior: "smooth",
+//                 block: "start",
+//             });
+//         })
+//     }
+// });
+
+$(document).ready(function() {
+    $('a[href*="#"]').click(function() {
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 1000,
+            easing: "swing"
+        });
+        return false;
+    });
 });
