04763472cf32900559726742bb7b221a59b8ef2f
Булат <hufdgdgdgfd@mail.ru>
2020-12-10 21:00:13 +0300

    fix 6

--- binpart/script/calculator.js
+++ binpart/script/calculator.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     let $element = $('input[type="range"]');
     let $calcCount = $('#calc-count .calc-count__res');
 
@@ -17,4 +18,33 @@ $(document).ready(function() {
     function updateHandle() {
         $calcCount.text($calcCount.text() * $element.val());
     }
+
+    let calcActiveWrap = $(".rangeslider__fill");
+    calcActiveWrap.append("<div id='calc-active'><img src='img/calcActive.svg' alt='calcActive' style></div>");
+
+    let calcActive = $('#calc-active');
+
+    let calcActiveImg = $('#calc-active img');
+
+
+
+    function calcCalcActiveWidth () {
+        calcActive.width($('#js-rangeslider-0').outerWidth());
+        calcActiveWrap.height(calcActiveImg.outerHeight());
+            calcActiveImg.on('load', function() {
+            calcActiveWrap.height($(this).outerHeight());
+        });
+    }
+
+    $(window).resize(function() {
+        calcCalcActiveWidth();
+    });
+
+    calcCalcActiveWidth();
+
+    let img = $('#calc-active img');
+    img.onload = function(){
+        alert( img.height );
+    }
+
 });
